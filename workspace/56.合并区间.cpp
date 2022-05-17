9e7ea860d646b9d580be2f92d82f8c5a793e3d99
hyyyyy2019 <47021303+hyyyyy2019@users.noreply.github.com>
2022-05-17 19:16:58 +0800

    interval

--- workspace/56.合并区间.cpp
+++ workspace/56.合并区间.cpp
@@ -7,8 +7,35 @@
 // @lc code=start
 class Solution {
 public:
+    // 从开始早的开始, 扩散cover范围
+    bool static cmp(vector<int> l, vector<int> r){
+        // 其实没必要
+        if(l[0]==r[0])
+            return l[1] < r[1];
+        return l[0] < r[0];
+    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
+        sort(intervals.begin(), intervals.end(), cmp);
+        vector<vector<int> > ans;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for(int i=1; i<intervals.size(); i++){
+            // 可以合并
+            if(intervals[i][0] <= end){
+                end = max(end, intervals[i][1]);
+            }
+            // 不可以合并
+            else{
+                // 保存之前的合并结果
+                ans.push_back({start, end});
+                // 更新start, end
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        // last one
+        ans.push_back({start, end});
+        return ans;
     }
 };
 // @lc code=end
