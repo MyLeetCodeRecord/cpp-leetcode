aae032cd3a6ceb4cc4b995d05889ad83f01a8a25
hyyyyy2019 <47021303+hyyyyy2019@users.noreply.github.com>
2022-05-20 21:52:41 +0800

    0520

--- markdown/STL.md
+++ markdown/STL.md
@@ -4,3 +4,8 @@
 
 ##### queue
 `que.insert(it, val)`: 在指定位置loc前插入值为val的元素, 返回指向这个元素的迭代器
+
+
+##### map
+`iterator lower_bound(const key_type &key)`: 返回一个迭代器, 指向键值 >=key 的第一个元素
+`iterator upper_bound(const key_type &key)`: 返回一个迭代器, 指向键值 > key 的第一个元素
---
+++ workspace/436.寻找右区间.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=436 lang=cpp
+ *
+ * [436] 寻找右区间
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        // start[i] <-> i
+        map<int, int> mp;
+        int index = 0;
+        for(auto interval: intervals)
+            mp[interval[0]] = index++;
+        
+        vector<int> result;
+        for(auto interval: intervals) {
+            auto it = mp.lower_bound(interval[1]);
+            if(it == mp.end())
+                result.push_back(-1);
+            else
+                result.push_back((*it).second);     // 使用map找到对应的index
+        }
+        return result;
+    }
+};
+// @lc code=end
+
