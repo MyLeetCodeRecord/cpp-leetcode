7d5495e6d3e8b6e56ade3315a790bd13a4f87fe9
hyyyyy2019 <47021303+hyyyyy2019@users.noreply.github.com>
2022-06-06 18:47:06 +0800

    0606

---
+++ workspace/398.随机数索引.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=398 lang=cpp
+ *
+ * [398] 随机数索引
+ */
+
+// @lc code=start
+class Solution {
+public:
+    Solution(vector<int>& nums) {
+
+    }
+    
+    int pick(int target) {
+
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
+// @lc code=end
+
