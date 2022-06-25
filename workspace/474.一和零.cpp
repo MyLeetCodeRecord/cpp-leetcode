b19f7e4bde2726ab404747e9ed4b523a50508711
hyyyyy2019 <47021303+hyyyyy2019@users.noreply.github.com>
2022-06-25 15:50:57 +0800

    0625

--- workspace/474.一和零.cpp
+++ workspace/474.一和零.cpp
@@ -10,7 +10,7 @@ public:
     // dp[j][k]: j个0和k个1的最大子集size
     int findMaxForm(vector<string>& strs, int m, int n) {
         vector<vector<int> > dp(m+1, vector<int>(n+1, 0));
-        for(int i=0; i<strs.size(); i++){   // Item
+        for(int i=0; i<strs.size(); i++){   // Items
             int zeroNum = 0;
             int oneNum = 0;
             for(char ch: strs[i]){
@@ -19,8 +19,9 @@ public:
                 else
                     oneNum++;
             }
-            for(int j=m; j>=zeroNum; j--){  // Capacity
-                for(int k=n; k>=oneNum; k--){   // Capacity
+            // 没有先后顺序, 但是不能"分开"考虑
+            for(int j=m; j>=zeroNum; j--){  // Capacity for 0
+                for(int k=n; k>=oneNum; k--){   // Capacity for 1
                     dp[j][k] = max(dp[j][k], dp[j-zeroNum][k-oneNum]+1);
                 }
             }
