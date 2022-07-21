9259d4499c56b0965a3a0feafd00d2415dba58f6
huyu <343921998@qq.com>
2022-07-21 15:05:50 +0800

    0721

--- markdown/专题 - DP - 双序列.md
+++ markdown/专题 - DP - 双序列.md
@@ -284,7 +284,7 @@ int minimumDeleteSum(string s1, string s2) {
 ```CPP
 int minInsertions(string s) {
     int n = s.size();
-    vector<vector<int> > dp(n, vector<int>(s.size(), 0));
+    vector<vector<int> > dp(n, vector<int>(n, 0));
     for(int i=n-1; i>=0; i--){
         for(int j=i+1; j<n; j++){
             if(s[i] == s[j]){
@@ -296,5 +296,25 @@ int minInsertions(string s) {
         }
     }
     return dp[0][n-1];
+}
+```
+
+> 这道题也可以借助`LCS`, 求字符串s及逆序字符串t的最长公共子序列长度, 要删除的次数即为`n-LCS`
+
+```CPP
+int minInsertions(string s) {
+    string t = s;
+    reverse(s.begin(), s.end());
+    int n = s.size();
+    vector<vector<int> > dp(n+1, vector<int>(n+1, 0));
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(s[i-1] == t[j-1])
+                dp[i][j] = dp[i-1][j-1]+1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return n - dp[n][n];
 }
 ```
--- workspace/1312.让字符串成为回文串的最少插入次数.cpp
+++ workspace/1312.让字符串成为回文串的最少插入次数.cpp
@@ -7,21 +7,38 @@
 // @lc code=start
 class Solution {
 public:
+    // 借助LCS
     int minInsertions(string s) {
+        string t = s;
+        reverse(s.begin(), s.end());
         int n = s.size();
-        vector<vector<int> > dp(n, vector<int>(s.size(), 0));
-        for(int i=n-1; i>=0; i--){
-            for(int j=i+1; j<n; j++){
-                if(s[i] == s[j]){
-                    dp[i][j] = dp[i+1][j-1];
-                }
-                else{
-                    dp[i][j] = min(dp[i+1][j], dp[i][j-1]) + 1;
-                }
+        vector<vector<int> > dp(n+1, vector<int>(n+1, 0));
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        return dp[0][n-1];
+        return n - dp[n][n];
     }
+    // 直接DP
+    // int minInsertions(string s) {
+    //     int n = s.size();
+    //     vector<vector<int> > dp(n, vector<int>(n, 0));
+    //     for(int i=n-1; i>=0; i--){
+    //         for(int j=i+1; j<n; j++){
+    //             if(s[i] == s[j]){
+    //                 dp[i][j] = dp[i+1][j-1];
+    //             }
+    //             else{
+    //                 dp[i][j] = min(dp[i+1][j], dp[i][j-1]) + 1;
+    //             }
+    //         }
+    //     }
+    //     return dp[0][n-1];
+    // }
 };
 // @lc code=end
 
