14111687bcaa8a50f9a4ec52ded4176b60f2a399
huyu <343921998@qq.com>
2022-09-12 23:46:27 +0800

    0912

--- acwing/Section 1/3_binarysearch_float.cpp
+++ acwing/Section 1/3_binarysearch_float.cpp
@@ -5,7 +5,7 @@ using namespace std;
 double n;
 
 // 二分法模板の浮点数二分, 唯一需要改变的是check()函数
-int binary_search(double n, double l, double r){
+double binary_search(double n, double l, double r){
     while(r-l > 1e-8){  // 多取几位
         double mid = (l+r)/2;
         // 体会一下"纯搜索", 不用+1 or +1e(-x)处理边界
