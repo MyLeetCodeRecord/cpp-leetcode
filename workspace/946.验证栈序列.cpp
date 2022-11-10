299a42b89f3f5cd31e98135e3cae404ca70c25d6
maggie <343921998@qq.com>
2022-11-10 09:19:36 +0800

    1110

---
+++ workspace/946.验证栈序列.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=946 lang=cpp
+ *
+ * [946] 验证栈序列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        if(pushed.size()!=popped.size())
+            return false;
+        stack<int> sk;
+        int j = 0;  // popped中的指针
+        for(int i=0; i<pushed.size(); i++){
+            sk.push(pushed[i]);
+            while(!sk.empty() && sk.top() == popped[j]){
+                sk.pop();
+                j++;
+            }
+        }
+        return j==popped.size();
+    }
+};
+// @lc code=end
+
