08249fd39c8a9c5c7d8f9199d2b81b2f4e4346e6
maggie <343921998@qq.com>
2022-11-28 14:07:24 +0800

    1128

--- workspace/813.最大平均值和的分组.cpp
+++ workspace/813.最大平均值和的分组.cpp
@@ -8,28 +8,22 @@
 class Solution {
 public:
     double largestSumOfAverages(vector<int>& nums, int K) {
-        int N = nums.size();
-        // 累计和 => 用于求区间和(平均值)
-        vector<double> cumSum(N+1, 0.0);
-        for(int i=1; i<=N; i++){
-            cumSum[i] = cumSum[i-1] + nums[i-1];
+        int n = nums.size();
+        vector<double> prefixSum(n+1, 0);
+        for(int i=1; i<=n; i++){
+            prefixSum[i] = prefixSum[i-1]+nums[i-1];
         }
-        // 区间dp[i][k]
-        vector<vector<double> > dp(N+1, vector<double>(K+1, 0));
-        dp[0][0] = 0;
-        for(int i=1; i<=N; i++){
-            dp[i][1] = cumSum[i] / i;
-            for(int k=2; k<=min(K, i); k++){
-                for(int j=k-1; j<i; j++){
-                    dp[i][k] = max(dp[i][k], dp[j][k-1]+(cumSum[i]-cumSum[j])/(i-j));
+        // dp[i][k]: 将0~i划分为最多k个子数组的最大分数
+        vector<vector<double>> dp(n+1, vector<double>(K+1, 0));
+        for(int i=1; i<=n; i++){
+            dp[i][1] = prefixSum[i] / i;  // 没有初始化会出错
+            for(int k=2; k<=K; k++){
+                for(int j=0; j<i; j++){
+                    dp[i][k] = max(dp[i][k], dp[j][k-1] + (prefixSum[i]-prefixSum[j])/(i-j));
                 }
             }
         }
-        double ans = 0.0;
-        for(int k=1; k<=K; k++){
-            ans = max(ans, dp[N][k]);
-        }
-        return ans;
+        return dp[n][K];
     }
 };
 // @lc code=end
